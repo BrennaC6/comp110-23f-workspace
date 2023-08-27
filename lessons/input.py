13a0ba18fd41c7b32a9ee16fea2ad79bbcdad2ff
Brenna Curtis <bcurtis@unc.edu>
2023-08-27 18:10:26 -0400

    printed things

---
+++ lessons/input.py
@@ -0,0 +1,5 @@
+"""Demonstrates asking user for input"""
+
+user_name: str= input("What is your name? "r)
+print("Hello, " + user_name + ", good morning")
+print ("You are the best programmer, " + user_name)
